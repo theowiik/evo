ca3652602a8d4c7ca80b28758e7bbc8d64ca2bbf
theowiik <42991003+theowiik@users.noreply.github.com>
2020-06-08 20:16:40 +0200

    Generate a random world and display on the evo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TIMEBOX_ADDRESS = "11:75:68:ef:30:64";
-var btSerial = new (require("bluetooth-serial-port").BluetoothSerialPort)();
-var Divoom = require("node-divoom-timebox-evo");
+let btSerial = new (require("bluetooth-serial-port").BluetoothSerialPort)();
+let Divoom = require("node-divoom-timebox-evo");
 let Jimp = require("jimp");
 
 /**
@@ -11,17 +11,8 @@ const PIXELS = 16;
 /**
  * The hex color of alive cells.
  */
-const ALIVE_COLOR = 0xffffffff;
+const ALIVE_COLOR = 0x42f59eff;
 const IMAGE_NAME = "frame";
-
-let image = new Jimp(PIXELS, PIXELS, "black", (err, image) => {
-  if (err) throw err;
-});
-
-image.setPixelColor(ALIVE_COLOR, 1, 1);
-
-let file = `${IMAGE_NAME}.${image.getExtension()}`;
-image.write(file);
 
 btSerial.findSerialPortChannel(
   TIMEBOX_ADDRESS,
@@ -31,12 +22,7 @@ btSerial.findSerialPortChannel(
       channel,
       function () {
         console.log("connected");
-
-        writeImage();
-
-        btSerial.on("data", function (buffer) {
-          console.log(buffer.toString("ascii"));
-        });
+        gameOfLife();
       },
       function () {
         console.log("cannot connect");
@@ -48,9 +34,9 @@ btSerial.findSerialPortChannel(
   }
 );
 
-function writeImage() {
+function uploadFrame() {
   var d = new Divoom.TimeboxEvo().createRequest("picture");
-  d.read(IMAGE_NAME + ".png")
+  d.read("frame.png")
     .then(result => {
       console.log(result.asBinaryBuffer());
       result.asBinaryBuffer().forEach(elt => {
@@ -64,6 +50,64 @@ function writeImage() {
       console.log("uploaded image");
     })
     .catch(err => {
-      throw err;
+      console.log(err);
     });
+}
+
+async function gameOfLife() {
+  while (true) {
+    let world = randomWorld();
+    await worldToFrame(world);
+    uploadFrame();
+    await sleep(500);
+  }
+}
+
+/**
+ * Generates a random world.
+ *
+ * @returns a matrix of booleans with the width and height of the PIXELS variable.
+ */
+function randomWorld() {
+  let world = [];
+  const p = 0.2;
+
+  for (let rowIndex = 0; rowIndex < PIXELS; rowIndex++) {
+    let row = [];
+
+    for (let colIndex = 0; colIndex < PIXELS; colIndex++) {
+      row.push(Math.random() <= p);
+    }
+
+    world.push(row);
+  }
+
+  return world;
+}
+
+function worldToFrame(w) {
+  let image = new Jimp(PIXELS, PIXELS, "black", (err, image) => {
+    if (err) throw err;
+  });
+
+  for (let rowIndex = 0; rowIndex < PIXELS; rowIndex++) {
+    for (let colIndex = 0; colIndex < PIXELS; colIndex++) {
+      if (w[rowIndex][colIndex]) image.setPixelColor(ALIVE_COLOR, rowIndex, colIndex);
+    }
+  }
+
+  let file = `${IMAGE_NAME}.${image.getExtension()}`;
+
+  return image
+    .writeAsync(file)
+    .then(() => {
+      console.log("saved image");
+    })
+    .catch(err => {
+      console.log("could not save image");
+    });
+}
+
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
 }
