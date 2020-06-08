ba57fe5e9ed349b5fc10550ba863d982d95c193c
theowiik <42991003+theowiik@users.noreply.github.com>
2020-06-08 17:48:08 +0200

    Write a image and send to the evo

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const TIMEBOX_ADDRESS = "11:75:68:ef:30:64";
 var btSerial = new (require("bluetooth-serial-port").BluetoothSerialPort)();
 var Divoom = require("node-divoom-timebox-evo");
+let Jimp = require("jimp");
+
+/**
+ * The width of the (square) matrix.
+ */
+const PIXELS = 16;
+
+/**
+ * The hex color of alive cells.
+ */
+const ALIVE_COLOR = 0xffffffff;
+const IMAGE_NAME = "frame";
+
+let image = new Jimp(PIXELS, PIXELS, "black", (err, image) => {
+  if (err) throw err;
+});
+
+image.setPixelColor(ALIVE_COLOR, 1, 1);
+
+let file = `${IMAGE_NAME}.${image.getExtension()}`;
+image.write(file);
 
 btSerial.findSerialPortChannel(
   TIMEBOX_ADDRESS,
@@ -29,7 +50,7 @@ btSerial.findSerialPortChannel(
 
 function writeImage() {
   var d = new Divoom.TimeboxEvo().createRequest("picture");
-  d.read("img.png")
+  d.read(IMAGE_NAME + ".png")
     .then(result => {
       console.log(result.asBinaryBuffer());
       result.asBinaryBuffer().forEach(elt => {
